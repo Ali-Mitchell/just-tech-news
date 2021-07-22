bd65aa0dfe26810cfe91b9ba54f24ec1cd7d85c3
AliMitchell <ali.mitchell.n@gmail.com>
2021-07-22 12:03:58 -0600

    fixing bcrypt errors, changing styling, and troubleshooting add post errors

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const sequelize = require('../config/connection');
+const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
@@ -88,3 +88,4 @@ router.get('/edit/:id', withAuth, (req, res) => {
 
 
 module.exports = router;
+
